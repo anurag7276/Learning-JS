107feee1ed6a00987068ecc849d9c0e5d68e9483
anurag7276 <161753889+anurag7276@users.noreply.github.com>
2024-04-05 19:50:24 +0530

    Create dataTypes.js

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"  //trear all JS code as new version
+
+// alert(3+3)  // we are using node js not browser
+
+// semicolun use to improve code readability  should be high
+
+let name = "anurag"
+let age  = 18
+let isLoggedIn = false
+
+// number => 2 to power 53  range 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => not yet assigned
+// symbol => unique
+
+
+// object
+  
+console.log(typeof undefined); //  undefined
+console.log(typeof null); //  object
