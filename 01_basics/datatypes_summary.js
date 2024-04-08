60f43ce88694d8f982cff890cdbf15eb0a62ff4b
anurag7276 <161753889+anurag7276@users.noreply.github.com>
2024-04-08 18:05:34 +0530

    Create datatypes_summary.js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,16 @@
+// Primitive
+
+// 7 types : string, Number, Boolean, null ,symbol ,BigInt
+
+// Reference ( Non primitive)
+ 
+// array , Objects , Fuctions   =>//object hote hai
+
+// JS is dynamically Typed language
+
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id===anotherid); //false
+
